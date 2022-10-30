d9033c1d487cc5568e1c24750ae106b25be24250
cuong2002k <masenkogl@gmail.com>
2022-10-30 20:27:53 +0700

    home,Detailsproduct

---
+++ CozaStore/Controllers/CozaHomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CozaStore.Models;
+namespace CozaStore.Controllers
+{
+    public class CozaHomeController : Controller
+    {
+        CozaStoreEntities db = new CozaStoreEntities();
+        // GET: CozaHome
+        public ActionResult Index()
+        {
+
+            return View(db.Product.ToList());
+        }
+        [ChildActionOnly]
+        public ActionResult TypeShose()
+        {
+            return PartialView(db.Category.ToList());
+        }
+        [ChildActionOnly]
+        public ActionResult Banner()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ProductDetails(int? productid)
+        {
+            Product product = db.Product.SingleOrDefault(n => n.Productid == productid);
+            if(product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
