517710a3ee0905cb5caf6b071ab7fff7e01bc49a
cuong2002k <masenkogl@gmail.com>
2022-11-17 21:53:24 +0700

    admin

--- CozaStore/Areas/Dashboard/Controllers/AdminController.cs
+++ CozaStore/Areas/Dashboard/Controllers/AdminController.cs
@@ -3,15 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using CozaStore.Models;
+using CozaStore.Areas.Dashboard.Models;
 namespace CozaStore.Areas.Dashboard.Controllers
 {
     public class AdminController : Controller
     {
+        CozaStoreEntities db = new CozaStoreEntities();
         // GET: Dashboard/Admin
         public ActionResult Index()
         {
+            var sum = db.Order.Sum(n => n.TotalPrice);
+            var cntorder = db.Order.ToList();
+            var cntuser = db.User.ToList();
+            ViewBag.sum = sum;
+            ViewBag.cntorder = cntorder.Count();
+            ViewBag.cntuser = cntuser.Count();
             return View();
+        }
+       [ChildActionOnly]
+       public ActionResult ViewTopProduct()
+        {
+            var lstproduct = (from c in db.Product
+                             select c).ToList().Take(6);
+
+            List<Rankproduct> list = new List<Rankproduct>();
+            foreach (var item in lstproduct)
+            {
+                int count = 0;
+                var lstdetail = from c in db.DetailsOrder
+                                where c.Productid == item.Productid
+                                select c;
+                if(lstdetail != null)
+                count = lstdetail.Count();
+                Rankproduct tmp = new Rankproduct { product = item, numproduct = count };
+                list.Add(tmp);
+            }
+             
+            return View(list);
+        }
+        [ChildActionOnly]
+        public ActionResult chart()
+        {
+            List<decimal?> totalprice = new List<decimal?>();
+            for(int i =1; i <= 12; i++)
+            {
+                var item = db.Order.Where(n => n.OrderDay.Value.Month == i).Sum(c => c.TotalPrice);
+                if (item != null)
+                    totalprice.Add(item);
+                else
+                    totalprice.Add(0);
+            }
+                
+            return View(totalprice);
+        }
+        [ChildActionOnly]
+        public ActionResult ViewRankUser()
+        {
+            var rankUser = db.User.ToList();
+            List<RankUser> lstrank = new List<RankUser>();
+            decimal? sum = 0;
+            foreach(var item in rankUser)
+            {
+                decimal? sumitem = db.Order.Where(n => n.Userid == item.Userid).Sum(n => n.TotalPrice);
+                lstrank.Add(new RankUser
+                {
+                    user = item,
+                    total = sumitem
+                });
+                sum += sumitem;
+            }
+            return View(lstrank);
+        }
+        [ChildActionOnly]
+        public ActionResult Order()
+        {
+            var lstorder = from c in db.Order
+                           join p in db.User
+                           on c.Userid equals p.Userid
+                           select new UserOrder
+                           {
+                               user = p,
+                               order = c
+                           };
+            return View(lstorder.ToList());
         }
     }
 }
--- CozaStore/AssetAdmin/dist/js/dashboard1.js
+++ CozaStore/AssetAdmin/dist/js/dashboard1.js
@@ -1,56 +1,56 @@
-$(function() {
-    "use strict";
+
+    $(function () {
+        "use strict";
     // Dashboard 1 Morris-chart
     Morris.Area({
         element: 'morris-area-chart',
-        data: [{
-            period: '2010',
-            iphone: 0,
-            ipad: 0,
-            itouch: 0
-        }, {
-            period: '2011',
-            iphone: 130,
-            ipad: 100,
-            itouch: 80
-        }, {
-            period: '2012',
-            iphone: 80,
-            ipad: 60,
-            itouch: 70
-        }, {
-            period: '2013',
-            iphone: 70,
-            ipad: 200,
-            itouch: 140
-        }, {
-            period: '2014',
-            iphone: 180,
-            ipad: 150,
-            itouch: 140
-        }, {
-            period: '2015',
-            iphone: 105,
-            ipad: 100,
-            itouch: 80
-        }, {
-            period: '2016',
-            iphone: 250,
-            ipad: 150,
-            itouch: 200
-        }],
-        xkey: 'period',
-        ykeys: ['iphone', 'ipad'],
-        labels: ['iPhone', 'iPad'],
-        pointSize: 0,
-        fillOpacity: 0,
-        pointStrokeColors: ['#f62d51', '#7460ee', '#009efb'],
-        behaveLikeLine: true,
-        gridLineColor: '#f6f6f6',
-        lineWidth: 1,
-        hideHover: 'auto',
-        lineColors: ['#009efb', '#7460ee', '#009efb'],
-        resize: true
+            data: [{
+        period: '2010',
+        Shoe: 0,
+        Total: 0,
+        itouch: 0
+            }, {
+        period: '2021',
+        Shoe: 130,
+        Total: 100,
+        itouch: 80
+            }, {
+        period: '2012',
+        Shoe: 80,
+        Total: 60,
+        itouch: 70
+            }, {
+        period: '2013',
+        Shoe: 70,
+        Total: 200,
+        itouch: 140
+            }, {
+        period: '2014',
+        Shoe: 180,
+        Total: 150,
+        itouch: 140
+            }, {
+        period: '2015',
+        Shoe: 105,
+        Total: 100,
+        itouch: 80
+            }, {
+        period: '2016',
+        Shoe: 250,
+        Total: 150,
+        itouch: 200
+    }],
+    xkey: 'period',
+    ykeys: ['Shoe', 'Total'],
+    labels: ['Shoe', 'Total'],
+    pointSize: 0,
+    fillOpacity: 0,
+    pointStrokeColors: ['#f62d51', '#7460ee', '#009efb'],
+    behaveLikeLine: true,
+    gridLineColor: '#f6f6f6',
+    lineWidth: 1,
+    hideHover: 'auto',
+    lineColors: ['#009efb', '#7460ee', '#009efb'],
+    resize: true
     });
-
 });
