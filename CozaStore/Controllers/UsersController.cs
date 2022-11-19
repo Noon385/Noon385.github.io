d62b8eb06948f5534ab38d8a3ec189d3c77d52da
cuong2002k <masenkogl@gmail.com>
2022-11-19 10:31:14 +0700

    update

---
+++ CozaStore/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CozaStore.Models;
+namespace CozaStore.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        CozaStoreEntities db = new CozaStoreEntities();
+        public ActionResult Information(int id)
+        {
+            var user = db.User.SingleOrDefault(n => n.Userid == id);
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Information(FormCollection f)
+        {
+            string FullName = f["FullName"];
+            string Email = f["Email"];
+            string Password = f["Password"];
+            DateTime BirthDay = DateTime.Parse(f["BirthDay"].ToString());
+            bool Gender = bool.Parse(f["Gender"]);
+            string PhoneNumber = f["PhoneNumber"];
+            string Address = f["Address"];
+            User user = db.User.SingleOrDefault(n => n.Gmail == Email);
+            user.FullName = FullName;
+            user.Gmail = Email;
+            user.Password = Password;
+            user.BirthDay = BirthDay;
+            user.Gender = Gender;
+            user.Phone = PhoneNumber;
+            user.Address = Address;
+            Session["User"] = user;
+            db.SaveChanges();
+            return RedirectToAction("Information",new {id = user.Userid });
+
+        }
+    }
+}
